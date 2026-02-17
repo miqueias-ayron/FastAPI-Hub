41b53133e7b98e1e21148a18d07ca5e5eb56d28e
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-02-16 23:34:57 -0300

    feat: busca por livro

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from fastapi-hub!")
+
+
+if __name__ == "__main__":
+    main()
--- projects/project_one/books.py
+++ projects/project_one/books.py
@@ -11,6 +11,8 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
-@app.get('/books')
-async def read_all_books():
-    return BOOKS
+@app.get('/books/{book_title}')
+async def read_book(book_title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == book_title.casefold():
+            return book
