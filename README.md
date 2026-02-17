9f747f0afc87681ed62011af43f52c9394059d52
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-02-17 11:10:06 -0300

    feat: query parameters

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # FastAPI-Hub
+
+# Primeiro Endpoint (/books)
+
+![alt text](static/img/20260217-1341-41.2052606.gif)
+
+# Path Parameters ( Parâmetros de Caminho )
+
+![alt text](static/img/20260217-1332-43.8568389.gif)
+
+# Query Parameters ( Parâmetros de Busca )
+
+## Busca por Categoria
+![alt text](static/img/20260217-1337-10.1037759.gif)
+
+## Busca por Categoria com Parâmetro de Caminho
+![alt text](static/img/20260217-1404-06.3920370.gif)
--- projects/project_one/books.py
+++ projects/project_one/books.py
@@ -11,8 +11,28 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
+@app.get('/books')
+async def read_books():
+    return BOOKS
+
 @app.get('/books/{book_title}')
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
+        
+@app.get('/book')
+async def read_category_by_query(category:str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
+
+@app.get('/book/{book_author}/')
+async def read_category_by_query(book_author:str, category:str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return books_to_return
