5eb2ce91bd43a904188697a11f2a226464da2b0e
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-02-23 01:31:34 -0300

    feat: pydantic fields and configurations

--- projects/project_02/books2.py
+++ projects/project_02/books2.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -18,11 +19,24 @@ class Book:
         self.rating = rating
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = Field(description='O ID não precisa ser passado', default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
+
+    model_config = {
+        "json_schema_extra":{
+            "example":{
+                "title": "A new book",
+                "author": "New Author",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
+
+
     
 
 BOOKS = [
@@ -41,4 +55,9 @@ async def read_all_the_books():
 @app.post('/create-book')
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+    BOOKS.append(find_book_id(new_book))
+
+# funcao utilitaria para preenchimento automático de id
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
